efed1e1ddffb9ea88126711bf19adb1b18a32297
Ikuya Yamada <dalyson.roman345@gmail.com>
2020-04-25 20:08:47 +0900

    [add] function rejecting entry when empty

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
+    console.log('message: ' + msg);
   })
 });
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
-
